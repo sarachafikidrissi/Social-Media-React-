163b62d022f7502f86cf20036b953deb5226894a
Abdellah-Majdoul <abdellahmajdoul57@gmail.com>
2024-09-04 10:45:48 +0100

    login Profil

--- src/pages/LoginProfile/Components/login.jsx
+++ src/pages/LoginProfile/Components/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../../assets/images/friendwave-removebg-preview.png'
+
 const LoginPage = () => {
   const navigate =useNavigate()
   return (
@@ -55,27 +56,12 @@ const LoginPage = () => {
                     <div
                         class="w-full rounded-sm bg-[url('https://images.unsplash.com/photo-1449844908441-8829872d2607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxob21lfGVufDB8MHx8fDE3MTA0MDE1NDZ8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-center bg-no-repeat items-center">
                         {/* create  bg profil */}
-                        <div
-                            class="mx-auto flex justify-center w-[141px] h-[141px] bg-blue-300/20 rounded-full bg-[url('https://gem.ec-nantes.fr/wp-content/uploads/2019/01/profil-vide.png')] bg-cover bg-center bg-no-repeat">
+                        <div>
+                            <img src=''/>
+                            <input type='file' />
 
-                            <div class="bg-white/90 rounded-full w-6 h-6 text-center ml-28 mt-4">
-
-                                <input type="file" name="profile" id="upload_profile" hidden required/>
-
-                                <label for="upload_profile">
-                                        <svg data-slot="icon" class="w-6 h-5 text-blue-700" fill="none"
-                                            stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
-                                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z">
-                                            </path>
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z">
-                                            </path>
-                                        </svg>
-                                    </label>
-                            </div>
                         </div>
+                        
                         {/* uplode img */}
                         <div class="flex justify-end">
                             
