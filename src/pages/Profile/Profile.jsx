4d8df681bce968cef7a7e01f43e093dd7a907e64
Abdellah-Majdoul <abdellahmajdoul57@gmail.com>
2024-09-06 21:59:16 +0100

    profil post

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -20,11 +20,7 @@ const Profile = () => {
             <div >
               <div className='flex'>
                 <div className='w-[30vw] flex justify-center items-center'>
-                  <div className=' w-[60%] h-[85%] rounded-full'>{filterConnectedUser.profileImage ? (
-                    <img src={filterConnectedUser.profileImage} className='w-[15vw] h-[30vh] rounded-full'/>
-                  ) : (
-                    <div className="bg-white w-full h-full rounded-full" />
-                  )}</div>
+                  <div className=' w-[60%] h-[85%] rounded-full'></div>
                 </div>
                 <div className='w-[50vw] h-[45vh] flex items-center'>
                   <div className='flex flex-col gap-5'>
@@ -45,29 +41,36 @@ const Profile = () => {
               <h1 className='  w-14 pt-8 pb-10  m-auto'><span className='ms-[1rem]  text-2xl border-b-2 border-red-600 font-serif'>Posts</span></h1>
               <div className='flex gap-5 flex-wrap ms-[1rem]  bg-[#a19a9a7a] rounded-md'>
                 
-                  <div  className='w-[24vw] rounded-md'>
-                    <div className='bg-white flex gap-5 rounded-t-md border-b-4 p-2'>
-                      <div className='profil bg-[#fcfc] w-[4vw] h-[8vh] rounded-full'>
-                        {/* Display profile image */}
-                        {filterConnectedUser.profileImage ? (
-                          <img src={filterConnectedUser.profileImage}  className='w-full h-full rounded-full'/>
-                        ) : (
-                          <div className="bg-gray-200 w-full h-full rounded-full" />
-                        )}
-                      </div>
-                      <div className='nameProfil capitalize font-serif'>
-                        {filterConnectedUser.name} 
-                      </div>
+              <div className='flex gap-5 flex-wrap ms-[1rem] bg-[#a19a9a7a] rounded-md'>
+              {filterConnectedUser.userPost.map((post, index) => (
+                <div key={index} className='w-[24vw] rounded-md'>
+                  <div className='bg-white flex gap-5 rounded-t-md border-b-4 p-2'>
+                    <div className='profil bg-[#fcfc] w-[4vw] h-[8vh] rounded-full'>
+                      {filterConnectedUser.profileImage ? (
+                        <img
+                          src={filterConnectedUser.profileImage}
+                          className='w-full h-full rounded-full'
+                        />
+                      ) : (
+                        <div className="bg-gray-200 w-full h-full rounded-full" />
+                      )}
                     </div>
-                    <div className='w-[100%]'>
-                      <img className='h-[50vh] w-full bg-cover' src={filterConnectedUser.image}  />
-                    </div>
-                    <div className='bg-white w-[24vw] h-[10vh] rounded-b-md text-black flex justify-between items-center p-2'>
-                    <h1>{filterConnectedUser.userPost.likes} Like</h1>
-                    <h1>{filterConnectedUser.userPost.comments} Comments</h1>
-                      <h1>{filterConnectedUser.userPost.favorited}favorite</h1>
+                    <div className='nameProfil capitalize font-serif'>
+                      {filterConnectedUser.name}
                     </div>
                   </div>
+                  <div className='w-[100%]'>
+                    <img className='h-[50vh] w-full bg-cover' src={post.image} alt={`Post ${index + 1}`} />
+                  </div>
+                  <div className='bg-white w-[24vw] h-[10vh] rounded-b-md text-black flex justify-between items-center p-2'>
+                    <h1>{post.likes} Like</h1>
+                    <h1>{post.comments} Comments</h1>
+                    <h1>{post.favorited} Favorites</h1>
+                  </div>
+                </div>
+              ))}
+            </div>
+            
                 
               </div>
             </div>
